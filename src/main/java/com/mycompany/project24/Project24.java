95b398062065bfa495f1440d1f4e49845d7aa154
PedroMedeiros7631 <81970006+PedroMedeiros73423@users.noreply.github.com>
2024-09-23 19:50:14 -0300

    Add files via upload

---
+++ src/main/java/com/mycompany/project24/Project24.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.project24;
+
+public class Project24 {
+
+    public static void main(String[] args) {
+        int i = 12;
+        do{
+            i = i + 2;
+            System.out.println("É par: " + i);
+        }while(i < 22);
+        
+    }
+}
